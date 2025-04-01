2519b9e89965d1b883dcbbad96ad8df56aaf239b
midesweb <malvarez@desarrolloweb.com>
2025-04-01 12:44:38 +0200

    ir a la docu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
 
-            <p><a href="/docs">Ir a la documentación del API</a></p>
+            <p><a href="/docs">Ir a la documentación del API de timetables</a></p>
 
     </body>
 </html>
