9d1071fef3331622453e3298d6bc9a7b8497a1d4
midesweb <miguel@escuela.it>
2025-08-12 19:09:04 +0200

    remove docs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
 
-            <p><a href="/docs">Ir a la documentación del API de Timetables</a></p>
+            <p>API de Timetables!</p>
 
     </body>
 </html>
